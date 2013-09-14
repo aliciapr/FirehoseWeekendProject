749692dac52242087da0b258d12e2ce0d4269efe
aliciap <aliciap@aliciap-virtual-machine.(none)>
2013-09-14 16:17:28 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '82338ae77f520805457222fd59bfd2b237537177a203beae3357c39045d6fb972e732828ab23554226988d69cb5abc814881a8c0ffc14bcd95944cb13581c4f3'
