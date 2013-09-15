41b28350032316c026ed012030f1949f521c4e9f
aliciap <aliciap@aliciap-virtual-machine.(none)>
2013-09-15 14:32:34 -0400

    Making image uploading work

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,4 +1,9 @@
 class Pic < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :hour, :emotion, :learned
+  attr_accessible :hour, :emotion, :learned,
+
+  #add uploading functionality:
+:image
+
+mount_uploader :image, PictureUploader
 end
