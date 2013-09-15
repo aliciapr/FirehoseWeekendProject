952f5bf78365be4fc285f0c458fb419f989a3b0d
aliciap <aliciap@aliciap-virtual-machine.(none)>
2013-09-15 11:55:39 -0400

    Styled the top navbar and linked the pages

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,9 +1,11 @@
 class PicsController < ApplicationController
 def new
+#build a new model but don't save it	
 	@pic = Pic.new
 end
 
 def create
+#create a new model in the database	
 	@pic = Pic.create(params[:pic])
 	redirect_to pics_path
 end
